bdc994a249ebebbff79544fb9bfce0ff590c9e44
Your Name <you@example.com>
2026-07-29 14:38:54 +0530

    localpwa

---
+++ bubblewrap/app/src/main/java/com/onrenderg/vrdrive/DelegationService.java
@@ -0,0 +1,15 @@
+// Portions (c) Meta Platforms, Inc. and affiliates.
+package com.onrenderg.vrdrive;
+
+
+
+public class DelegationService extends
+        com.meta.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
